11387b64ba3a0dcab5ae327bc5e7eace306f68a4
Camila-Giraldo <gc.camila28@gmail.com>
2022-11-23 07:55:05 -0500

    EcmaScript y extensión .jsx

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Greeting, UserCard } from "./Greeting";
+// Se importa todo el módulo Product, no de forma individual como se hacía con las funciones de Greeting.
+import Product, { Navbar } from "./Product";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Greeting() {
-  function add(x, y) {
-    return x + y;
-  }
-
-  return <h1>{add(10, 30)}</h1>;
-}
+// Nota importante: Se puede agregar a los componentes de React la extensión .jsx como preferencia del desarrollador para diferenciar qué es código JS y cuál hace parte de React
 
 root.render(
   <>
     <Greeting />
-    <Greeting />
-    <Greeting />
-    <Greeting />
+    <UserCard />
+    <Product />
+    <Navbar />
   </>
 );
