83f453cc7c108c2cc2bab222ed138ae8dcb17c92
Camila-Giraldo <gc.camila28@gmail.com>
2022-11-28 18:38:46 -0500

    Otra forma de crear componentes en React

---
+++ src/Saludar.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+// No es el más recomendable para creación de componentes ya que es lo más viejo de React
+export class Saludar extends Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
